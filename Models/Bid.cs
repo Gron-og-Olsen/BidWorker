56e09eee143432b37e404a559d71945196a9dc1b
Cosmo99one <ruaengladaphirom@gmail.com>
2024-12-09 12:20:08 +0100

    Update

---
+++ Models/Bid.cs
@@ -0,0 +1,11 @@
+namespace BidWorker
+{
+    public class Bid
+    {
+        public int Id { get; set; }
+        public int AuctionId { get; set; }
+        public string BidderName { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime Timestamp { get; set; }
+    }
+}
